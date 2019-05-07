979b34e0c9db3d4f3d07d1b59c9d10838e7d265c
Venkataramireddy Bollavaram <venkataramireddy.b@HCL.COM>
2019-05-07 15:16:21 +0530

    Addede new Form Validation using required and minLength

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { FavoriteChangeEventArgs } from './favorite/favorite.component';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
+task = {
+  title: 'Review the Application',
+  assignee: {
+    name: 'Venkata Rami Reddy'
+}
+}
+
   dataList = [
     {
       'FirstName' : 'Roy',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,41 @@
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { SummaryPipe } from './summary.pipe';
 import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
+
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
+    SignupFormComponent,
     SummaryPipe,
     FavoriteComponent,
     PanelComponent,
-    LikeComponent
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    NewCourseFormComponent    
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthorsService,
